7c4edc6aabcdf970d3a82c064455e9176222252e
Carmen Del Ángel Martínez <dam_damc@informatica.mz>
2023-06-16 10:37:23 +0200

    Initial commit

---
+++ src/MatrizMZ.java
@@ -0,0 +1,53 @@
+
+public class MatrizMZ {
+
+    public int matrizcuadrada;
+    public int matriztriangular;
+
+    public MatrizMZ(int matrizcuadrada, int matriztriangular) {
+        this.matrizcuadrada = matrizcuadrada;
+        this.matriztriangular = matriztriangular;
+    }
+
+    public int getMatrizcuadrada() {
+        return matrizcuadrada;
+    }
+
+    public void setMatrizcuadrada(int matrizcuadrada) {
+        this.matrizcuadrada = matrizcuadrada;
+    }
+
+    public int getMatriztriangular() {
+        return matriztriangular;
+    }
+
+    public void setMatriztriangular(int matriztriangular) {
+        this.matriztriangular = matriztriangular;
+    }
+
+    public boolean Matriz(int n, int m){
+
+        for (int i = n; i >=0; i++){
+            System.out.println("Introduce el valor de n:" + n);
+            return true;
+        }
+        for (int i = m; i >= 0; i++){
+            System.out.println("Introduce el valor de m:" + m);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean esCuadrada(int m, int n){
+        if (n == m){
+            return true;
+        }return false;
+    }
+
+    public int esTriangular(){
+        if (){
+
+        }
+    }
+
+}
---
+++ src/MatrizMZTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrizMZTest {
+
+    @Test
+    void matriz() {
+        MatrizMZ matriz = new MatrizMZ;
+        matriz.
+    }
+
+    @Test
+    void esCuadrada() {
+    }
+
+    @Test
+    void esTriangular() {
+    }
+}
